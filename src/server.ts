e4c6c55d81dd219152ea2975b9d3fb14808340ae
Rudy Fraser <him@rudyfraser.com>
2023-06-22 07:19:14 -0400

    Turn back on

--- src/server.ts
+++ src/server.ts
@@ -63,7 +63,7 @@ export class FeedGenerator {
 
   async start(): Promise<http.Server> {
     await migrateToLatest(this.db)
-    // this.firehose.run()
+    this.firehose.run()
     this.server = this.app.listen(this.cfg.port)
     await events.once(this.server, 'listening')
     return this.server
