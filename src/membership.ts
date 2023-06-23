4116c61a6653f2fe85844f81449adacd63384330
Rudy Fraser <him@rudyfraser.com>
2023-06-23 10:38:04 -0400

    Add user

--- src/membership.ts
+++ src/membership.ts
@@ -520,6 +520,7 @@ export const DID_ADDITIONS = [
   'did:plc:wmr57zmu5nhy6snnamjrxgnc',
   'did:plc:mjci563kfkhyniuwepdwcybb',
   'did:plc:jdbf2u27g7lubwszld66frz2',
+  'did:plc:bnmuj3kdepypfc6uv3ilzfbz',
 ]
 
 export const DID_REMOVALS = [
