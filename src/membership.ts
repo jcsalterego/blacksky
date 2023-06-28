a9d5c3af0d7b71d662a3522223ec5a5085674ee0
Rudy Fraser <him@rudyfraser.com>
2023-06-27 20:26:26 -0400

    Add user

--- src/membership.ts
+++ src/membership.ts
@@ -610,6 +610,7 @@ export const DID_ADDITIONS = [
   'did:plc:j2e3etalu6otgeecfcpddbju',
   'did:plc:qgeqpdif4lay7estnqx7zuwe',
   'did:plc:ksk5hz2q25v6hnqscipzbdf5',
+  'did:plc:jo54lltmuomqx6n2trpcaaf5',
   // 'did:plc:bnmuj3kdepypfc6uv3ilzfbz', Asked to be removed after
 ]
 
